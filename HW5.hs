578edba1457682fa1d5355b1067222709b848638
Dar Levy <darlevy8@gmail.com>
2024-06-16 14:51:10 +0300

    Implement all functions

--- HW5.hs
+++ HW5.hs
@@ -30,18 +30,41 @@ foldMap' :: (Foldable t, Monoid m) => FoldMapFunc a m result -> t a -> result
 foldMap' FoldMapFunc{agg, finalize} = finalize . foldMap agg
 
 -- Section 1: Foldable functions
-fmsum :: Num a => FoldMapFunc a _ a
-fmor :: FoldMapFunc Bool _ Bool
-fmfold :: Monoid a => FoldMapFunc a _ a
-fmelem :: Eq a => a -> FoldMapFunc a _ Bool
-fmfind :: (a -> Bool) -> FoldMapFunc a _ (Maybe a)
-fmlength :: FoldMapFunc a _ Int
-fmnull :: FoldMapFunc a _ Bool
-fmmaximum :: Ord a => FoldMapFunc a _ (Maybe a)
-fmminimum :: Ord a => FoldMapFunc a _ (Maybe a)
-fmmaxBy :: Ord b => (a -> b) -> FoldMapFunc a _ (Maybe a)
-fmminBy :: Ord b => (a -> b) -> FoldMapFunc a _ (Maybe a)
-fmtoList :: FoldMapFunc a _ [a]
+fmsum :: Num a => FoldMapFunc a (Sum a) a
+fmsum = FoldMapFunc Sum getSum
+
+fmor :: FoldMapFunc Bool Any Bool
+fmor = FoldMapFunc Any getAny
+
+fmfold :: Monoid a => FoldMapFunc a a a
+fmfold = FoldMapFunc id id
+
+fmelem :: Eq a => a -> FoldMapFunc a Any Bool
+fmelem x = FoldMapFunc (Any . (== x)) getAny
+
+fmfind :: (a -> Bool) -> FoldMapFunc a (First a) (Maybe a)
+fmfind p = FoldMapFunc (First . (\x -> if p x then Just x else Nothing)) getFirst
+
+fmlength :: FoldMapFunc a (Sum Int) Int
+fmlength = FoldMapFunc (const 1) getSum
+
+fmnull :: FoldMapFunc a All Bool
+fmnull = FoldMapFunc (const False) not . getAll
+
+fmmaximum :: Ord a => FoldMapFunc a (Max (Maybe a)) (Maybe a)
+fmmaximum = FoldMapFunc (Max . Just) getMax
+
+fmminimum :: Ord a => FoldMapFunc a (Min (Maybe a)) (Maybe a)
+fmminimum = FoldMapFunc (Min . Just) getMin
+
+fmmaxBy :: Ord b => (a -> b) -> FoldMapFunc a (Max (Maybe (b, a))) (Maybe a)
+fmmaxBy f = FoldMapFunc (\x -> Max (Just (f x, x))) (fmap snd . getMax)
+
+fmminBy :: Ord b => (a -> b) -> FoldMapFunc a (Min (Maybe (b, a))) (Maybe a)
+fmminBy f = FoldMapFunc (\x -> Min (Just (f x, x))) (fmap snd . getMin)
+
+fmtoList :: FoldMapFunc a [a] [a]
+fmtoList = FoldMapFunc (:[]) id
 
 -- Section 2: Deque instances (Don't forget to implement the instances in Deque.hs as well!)
 newtype DequeWrapper a = DequeWrapper (Deque a) deriving (Show, Eq)
